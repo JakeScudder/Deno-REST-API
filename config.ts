c42c3ce2b47fe3c484a3b5708a5c307bc26af95e
Jake Scudder <scudsterr@gmail.com>
2020-07-10 16:44:07 -0400

    DB connection
    * Connected postgres db to api

---
+++ config.ts
@@ -0,0 +1,9 @@
+const dbCreds = {
+  user: "Jake",
+  database: "denoapi",
+  password: "123456",
+  hostname: "localhost",
+  port: 5000,
+};
+
+export { dbCreds };
--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
-import router from './routes.ts'
-const port = 7000;
+import { Application, Router } from "https://deno.land/x/oak/mod.ts"; // @ts-ignore
+import router from "./routes.ts"; // @ts-ignore
+const port = Deno.env.get("PORT") || 7000;
 
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server running on port ${port}`)
+console.log(`Server running on port ${port}`);
 
-await app.listen({ port })
+await app.listen({ port: +port });
